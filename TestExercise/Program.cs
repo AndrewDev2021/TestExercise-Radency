de3cfde850d4db555f943569576ff11380fa2047
Andrii Poberezhnik <andrepoper2016@gmail.com>
2022-07-21 19:52:32 +0300

    Added sorting with the same weight

--- TestExercise/Program.cs
+++ TestExercise/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
-
-namespace TestExercise;
+﻿namespace TestExercise;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var instance = new Recalculation();
-
-        Console.WriteLine($"{instance.Order(" 2022 70 123 3344 13 ")}" + // Valid data
-                          $"\n{instance.Order("45 34 24 108 76 58 64 130 80")}" +
-                          $"\n{instance.Order("45 34 24 d108 76 58 64 130 80")}" + //Invalid data
-                          $"\n{instance.Order("45 34 24 -108 76 58 64 130 80")}");
+        Console.WriteLine($"{Recalculation.Order("2000 11 11 10003 22 123 1234000")}" + // Valid data
+                          $"\n{Recalculation.Order("45 34 24 108 36 76 58 64 130 80")}" +
+                          $"\n{Recalculation.Order("    2022 70 123    3344 13 ")}" +
+                          $"\n{Recalculation.Order("45 34 24 d108 76 58 64 130 80")}" + //Invalid data
+                          $"\n{Recalculation.Order("45 34 24 -108 76 58 64 130 80")}");
     }
 }
--- TestExercise/Recalculation.cs
+++ TestExercise/Recalculation.cs
@@ -1,38 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestExercise;
+﻿namespace TestExercise;
 
 internal class Recalculation
 {
-    public string Order(string input)
+    public static string Order(string input)
     {
-        var tempList = input.Split(' ').Where(p => p != "").ToList();
+        var tempList = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
         var listOfTuples = new List<Tuple<string, int>>();
 
         foreach (var item in tempList)
         {
-            var temp3 = 0;
+            var temp = 0;
 
             foreach (var item2 in item.ToCharArray())
             {
                 var flag = int.TryParse(item2.ToString(), out int res);
 
                 if (flag && res >= 0)
-                    temp3 += res;
+                    temp += res;
                 else
                     return "invalid data";
             }
 
-            listOfTuples.Add(new Tuple<string, int>(item,temp3));
+            listOfTuples.Add(new Tuple<string, int>(item, temp));
         }
 
-        var sortArray = listOfTuples.OrderBy(p => p.Item2).Select(p => p.Item1).ToArray();
+        var sortListOfTuples = listOfTuples.OrderBy(p => p.Item2).ToList();
 
-        return string.Join(' ', sortArray);
+        var arrWeights = sortListOfTuples.Select(p => p.Item2).ToArray();
+        var arrValues = sortListOfTuples.Select(p => p.Item1).ToArray();
+
+        return string.Join(' ', SortWithSameWeights(arrWeights,arrValues));
+    }
+
+    private static bool CompareOfString(string str1, string str2)
+    {
+        int cmpVal = string.Compare(str1, str2, StringComparison.Ordinal);
+
+        if (cmpVal == 0)
+            return false;
+        if (cmpVal < 0)
+            return false;
+
+        return true;
+    }
+
+    private static string[] SortWithSameWeights(int[] array,string[] array2)
+    {
+        var len = array.Length;
+        for (var i = 1; i < len; i++)
+        {
+            for (var j = 0; j < len - i; j++)
+            {
+                if (array[j] == array[j + 1] && CompareOfString(array2[j], array2[j + 1]))
+                {
+                    Swap(ref array2[j], ref array2[j + 1]);
+                }
+            }
+        }
+        return array2;
+    }
+    private static void Swap(ref string e1, ref string e2)
+    {
+        (e1, e2) = (e2, e1);
     }
 }
+    
